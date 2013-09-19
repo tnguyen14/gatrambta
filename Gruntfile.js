6e03dfb36ea71a33ae65e4c00265ff43ba0835d8
Tri Nguyen <tri@tridnguyen.com>
2013-09-18 20:23:48 -0400

    pulling in data

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 // Generated on 2013-09-02 using generator-webapp 0.4.1
 'use strict';
-var LIVERELOAD_PORT = 35729;
+var LIVERELOAD_PORT = 36000;
 var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
 var mountFolder = function (connect, dir) {
     return connect.static(require('path').resolve(dir));
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,28 @@
 /*global define */
-define([], function () {
-    'use strict';
+define( function (require) {
+	'use strict';
 
-    return '\'Allo \'Allo!';
+	var $ = require('jquery');
+
+	var _getData = function() {
+		return $.ajax({
+			url: "/data/mbta.json"
+		});
+	};
+
+	var ready = function( cb ) {
+		_getData().done(function(data) {
+			cb(data);
+		});
+	}
+
+	var init = function() {
+		ready(function( data ) {
+			var $schedule = $(".schedule");
+
+		});
+	}
+	return {
+		init: init
+	}
 });
